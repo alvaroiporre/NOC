2e64af9f6f7f3f34f42ad27e82ec9cb48143a316
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-13 16:19:51 -0400

    Add log.repository.impl

--- src/domain/repository/log.repository.ts
+++ src/domain/repository/log.repository.ts
@@ -1,5 +1,5 @@
 import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
-export abstract class LogDatasource {
+export abstract class LogRepository {
   abstract saveLog( log: LogEntity): Promise<void>;
   abstract getLogs( severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
 }
