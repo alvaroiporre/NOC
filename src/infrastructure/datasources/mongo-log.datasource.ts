399057bf1b1b125003857542e36e2532c33224a6
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-14 21:43:15 -0400

    Add postgres to docker compose

--- src/infrastructure/datasources/mongo-log.datasource.ts
+++ src/infrastructure/datasources/mongo-log.datasource.ts
@@ -14,5 +14,4 @@ export class MongoLogDatasource implements LogDatasource {
     });
     return logs.map(LogEntity.fromObject); //return logs.map( mongoLog => LogEntity.fromObject(mongoLog) )
   }
-
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,26 +1,33 @@
 import { envs } from "../config/plugins/envs.plugin";
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { LogRepository } from "../domain/repository/log.repository";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SenEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const fileSystemLogRepository = new LogRepositoryImpl( new FileSystemDatasource());
+const logRepository = new LogRepositoryImpl( 
+  //new FileSystemDatasource()
+  new MongoLogDatasource(),
+  );
 const emailService = new EmailService();
 
 export class Server {
-  public static start() {
+  public static async start() {
     console.log("Server started...");
 
     //Mandar Email
     // new SenEmailLogs(emailService, fileSystemLogRepository).execute(['tecno_economica@hotmail.com', 'alvaroiporremartinez@gmail.com']);
-
+    
+    const logs = await logRepository.getLogs(LogSeverityLevel.high);
+    console.log(logs);
 
     // CronService.createJob("*/5 * * * * *", () => {
     //   const url = 'https://google.com';
-    //   new CheckService(fileSystemLogRepository,
+    //   new CheckService(logRepository,
     //     () => console.log('success'),
     //     (error) => console.log(error)
     //   ).execute(url);
