f3f2313d34789bd94c6721f087d982cd9471c9d3
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-14 20:44:47 -0400

    Add connection to mongoDB

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -7,5 +7,7 @@ export const envs = {
   MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
   MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
   MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+  MONGO_URL: env.get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString()
   
 };
