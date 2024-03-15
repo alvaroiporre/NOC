3316e97f19288a86a016e9d9aea0a1dc5197b14e
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-14 22:21:33 -0400

    Add postgres-log.datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -5,13 +5,15 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SenEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
 const logRepository = new LogRepositoryImpl( 
-  //new FileSystemDatasource()
-  new MongoLogDatasource(),
+  //new FileSystemDatasource(),
+  //new MongoLogDatasource(),
+  new PostgresLogDatasource(),
   );
 const emailService = new EmailService();
 
@@ -22,15 +24,13 @@ export class Server {
     //Mandar Email
     // new SenEmailLogs(emailService, fileSystemLogRepository).execute(['tecno_economica@hotmail.com', 'alvaroiporremartinez@gmail.com']);
     
-    const logs = await logRepository.getLogs(LogSeverityLevel.high);
-    console.log(logs);
 
-    // CronService.createJob("*/5 * * * * *", () => {
-    //   const url = 'https://google.com';
-    //   new CheckService(logRepository,
-    //     () => console.log('success'),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    // });
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = 'https://google.com';
+      new CheckService(logRepository,
+        () => console.log('success'),
+        (error) => console.log(error)
+      ).execute(url);
+    });
   }
 }
